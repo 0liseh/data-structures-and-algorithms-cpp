3fc823e717135a5195fab535ddc5339fa3edc85b
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2023-03-24 14:28:10 -0600

    right answer

---
+++ twoSums/twoSums_BigO_faster.java
@@ -0,0 +1,12 @@
+public class twoSums_BigO_faster {
+    public int[] twoSum(int[] nums, int target) {
+        int position1 = 0;
+        int position2 = 1;
+        
+        //algorithm
+
+        int[] array = {position1, position2};
+
+        return array;
+    }
+}
--- twoSums/twoSums_BigO_n_squared.java
+++ twoSums/twoSums_BigO_n_squared.java
@@ -1,11 +1,11 @@
 //Big-O: O(n^2)
-class Solution {
+class twoSums_BigO_n_squared{
     public int[] twoSum(int[] nums, int target) {
         int position1 = 0;
         int position2 = 1;
         for(int i = 0; i < nums.length; i++){
             for(int j = 1; j < nums.length - 1; j++){
-                if(nums[i] + nums[j] == target){
+                if((nums[i] + nums[j]) == target && i!=j){
                     position1 = i;
                     position2 = j;
                 }
