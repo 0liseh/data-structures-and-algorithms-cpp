1916fb7991cdcf06a20fa9b8ac54db4f963fd378
Olisehemeka Chukwuma <olisehemeka.chukwuma@gmail.com>
2023-03-24 14:18:25 -0600

    twosums

---
+++ twoSums/twoSums_O(n^2).java
@@ -0,0 +1,19 @@
+//Big-O: O(n^2)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int position1 = 0;
+        int position2 = 1;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 1; j < nums.length - 1; j++){
+                if(nums[i] + nums[j] == target){
+                    position1 = i;
+                    position2 = j;
+                }
+            }
+        }
+
+        int[] array = {position1, position2};
+
+        return array;
+    }
+}
